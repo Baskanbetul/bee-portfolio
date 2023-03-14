bd7b4dc2461fa0a0fd66a925b5b7d4be808d8864
Baskanbetul <btlbskn@hotmail.com>
2023-03-14 17:31:33 -0600

    Added useEffect for changing location functionality and created Footer component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 import "../styles/Navbar.css"
 // import ReorderIcon from "@material-ui/icons/Reorder";
 // import createSvgIcon from "@material-ui/icons/utils/createSvgIcon";
@@ -7,6 +7,12 @@ import { Icon } from '@iconify/react';
 
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const location = useLocation()
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
 
   return (
     <div className="navbar" id={expandNavbar ? "open" : "close"}>
