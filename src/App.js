d182bd3bcdf5b7aa75060e8ae24d0d17a7189793
Baskanbetul <btlbskn@hotmail.com>
2023-03-14 00:41:28 -0600

    Deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Portfolio <code>src/App.js</code> and save to reload.
+          Portfolio let s try it <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
