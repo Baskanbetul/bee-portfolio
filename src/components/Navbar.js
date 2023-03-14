d156aa62e6ab094d1d3187efc8a687253023f91a
Baskanbetul <btlbskn@hotmail.com>
2023-03-14 16:42:35 -0600

    Tried to use icons for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "../styles/Navbar.css"
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+  const [expandNavbar, setExpandNavbar] = useState(false);
   return (
     <div className="navbar">
       <div className="toggleButton">
-        <button>
-
+        <button 
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+         <ReorderIcon />
         </button>
       </div>
       <div className="links">
