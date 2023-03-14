5d2c4f770e50e750599a7b13ef619a739f5d30bb
Baskanbetul <btlbskn@hotmail.com>
2023-03-14 17:15:16 -0600

    Removed re-order icon from @material-ui due to not to support by React and added icon by iconify

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "../styles/Navbar.css"
-import ReorderIcon from "@material-ui/icons/Reorder";
+// import ReorderIcon from "@material-ui/icons/Reorder";
+// import createSvgIcon from "@material-ui/icons/utils/createSvgIcon";
+import { Icon } from '@iconify/react';
 
 function Navbar() {
   const [expandNavbar, setExpandNavbar] = useState(false);
+
   return (
-    <div className="navbar">
+    <div className="navbar" id={expandNavbar ? "open" : "close"}>
       <div className="toggleButton">
         <button 
           onClick={() => {
             setExpandNavbar((prev) => !prev);
           }}
         >
-         <ReorderIcon />
+         {/* <ReorderIcon /> */}
+        <Icon icon="material-symbols:reorder-rounded" vFlip={true} />
         </button>
       </div>
       <div className="links">
