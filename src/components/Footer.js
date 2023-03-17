3b6e4889ae2b9f526e5909ae35e54407f2b06d5b
Baskanbetul <btlbskn@hotmail.com>
2023-03-16 19:54:23 -0600

    Added div and p tag on Footer component to start using personal links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-      Footer
+    <div className="footer">
+      <div> </div>
+      <p> &copy; 2022 pedrotech.com </p>
     </div>
-  )
+  );
 }
 
 export default Footer
